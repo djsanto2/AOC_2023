1ee8078b76c9ffac795a0120fb558d13f62becc9
Daniel Santos <santosdanielj@gmail.com>
2023-12-30 13:13:11 -0500

    Completed day2 (disorganized)

--- day2/src/cube_counter.rs
+++ day2/src/cube_counter.rs
@@ -1,9 +1,23 @@
 use std::collections::VecDeque;
 use std::fs;
 use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+struct MyError(String);
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        write!(f, "Error Here: {}", self.0)
+    }
+}
 
 
-
+struct BagOfCubes{
+    red_ct: i32,
+    green_ct: i32,
+    blue_ct: i32,
+}
 
 pub fn read_doc(filename: &str) -> Result<Vec<String>, Box<dyn Error>>{
     
@@ -20,7 +34,7 @@ pub fn read_doc(filename: &str) -> Result<Vec<String>, Box<dyn Error>>{
 
 pub fn line_test(line: &str) -> Result<(bool,i32), Box<dyn Error>>{
     
-    let mut game_number = 0;
+    
     //let bag = bag_of_cubes{red_cubes_ct: 12,green_cubes_ct: 13,blue_cubes_ct:14};
 
     let mut line_deque: VecDeque<char> = VecDeque::new(); 
@@ -35,7 +49,7 @@ pub fn line_test(line: &str) -> Result<(bool,i32), Box<dyn Error>>{
     while *line_deque.front().unwrap() != ':'{
         current_digits.push(line_deque.pop_front().unwrap());
     }
-    game_number = current_digits.parse::<i32>().unwrap();
+    let game_number = current_digits.parse::<i32>().unwrap();
     line_deque.pop_front(); //pops the :
 
 
@@ -106,4 +120,92 @@ pub fn line_test(line: &str) -> Result<(bool,i32), Box<dyn Error>>{
     }
 
     Ok((true,game_number))
+}
+
+
+pub fn track_minimum_cubes(line: &str) -> Result<i32,Box<dyn Error>>{
+    
+    let line_deque: VecDeque<char> = str_to_deque(line).unwrap();
+
+    let bag_cube = count_max_cubes(line_deque).unwrap();
+
+    Ok(bag_cube.red_ct*bag_cube.blue_ct*bag_cube.green_ct)
+}
+
+fn str_to_deque(line: &str) -> Result<VecDeque<char>, Box<dyn Error>> {
+    let mut result_vec: VecDeque<char> = VecDeque::new();
+    for c in line.chars() {
+        result_vec.push_back(c);
+    }
+    Ok(result_vec)
+}
+
+impl Error for MyError{}
+
+fn count_max_cubes(mut line_deque:  VecDeque<char>) -> Result<BagOfCubes, Box<dyn Error>>{
+    //struct that tracks amount of cubes necessary
+    let mut cube_counter = BagOfCubes{
+        red_ct: 0,
+        green_ct: 0,
+        blue_ct: 0,
+    };
+
+    /*
+        After struggling with part 1's parsing I decided to work backwards across the 
+        line which allows me to use deque better. I did this as it is easier for me to
+        parse color->ct rather than ct->color as I will not have to store ct and update it
+        separately from final, can just use a struct to track all 3 color counts.
+
+        Update: turns out I will have to use insert to prepend to 'current_word'
+        so it will not be faster. ohy well I tried
+     */
+    let mut current_word: String = "".to_string();
+    while *line_deque.back().unwrap() != ':' && !line_deque.is_empty(){
+        let current_letter = line_deque.pop_back().unwrap();
+        if current_letter.is_numeric(){
+            if current_word == "red" {
+                current_word.clear();
+                current_word.push(current_letter);
+                //loop to get numbers with large amount of digits
+                while line_deque.back().unwrap().is_numeric() {
+                    current_word.insert(0,line_deque.pop_back().unwrap()); 
+                }
+                if cube_counter.red_ct <= current_word.parse::<i32>().unwrap() {
+                    cube_counter.red_ct = current_word.parse::<i32>().unwrap();
+                }
+            }
+            else if current_word == "blue" {
+                current_word.clear();
+                current_word.push(current_letter);
+                //loop to get numbers with large amount of digits
+                while line_deque.back().unwrap().is_numeric() {
+                    current_word.insert(0,line_deque.pop_back().unwrap()); 
+                }
+                if cube_counter.blue_ct <= current_word.parse::<i32>().unwrap() {
+                    cube_counter.blue_ct = current_word.parse::<i32>().unwrap();
+                }
+            }
+            else if current_word == "green" {
+                current_word.clear();
+                current_word.push(current_letter);
+                //loop to get numbers with large amount of digits
+                while line_deque.back().unwrap().is_numeric() {
+                    current_word.insert(0,line_deque.pop_back().unwrap()); 
+                }
+                if cube_counter.green_ct <= current_word.parse::<i32>().unwrap() {
+                    cube_counter.green_ct = current_word.parse::<i32>().unwrap();
+                }
+            }
+            else {
+                return Err(Box::new(MyError("Invalid word in string".into())));
+            }
+            current_word.clear()
+        } //if numeric
+        else if current_letter.is_alphabetic() {
+            current_word.insert(0,current_letter);
+        }
+        
+    }
+
+    Ok(cube_counter)
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,6 @@
 pub mod cube_counter;
 use cube_counter::*;
-
+use std::error::Error;
 
 fn main() {
     let lines = read_doc("input.txt").unwrap();
@@ -9,21 +9,39 @@ fn main() {
     //let mut gameNumber = 1;
 
     //println!("{:?}", lines);
-    let mut total = 0;
+    
     let mut line_edit: Vec<String>= Vec::new();
     for  line in lines{
         line_edit.push(line[5..].to_string());
     }
+    
+    println!("Total of the first part: {:?}", part1(line_edit.clone()).unwrap());
+
+    
+    println!("Total of the second part: {:?}", part2(line_edit.clone()).unwrap());
+    
+
+}
+
+fn part1(line_edit: Vec<String>) -> Result<i32, Box<dyn Error>>{
+    let mut total = 0;
     for line in line_edit{
         let result = cube_counter::line_test(&line).unwrap();
         if result.0 {
             total += result.1;
         }
     }
-    println!("{:?}", total);
-
-    //print!("{:?}", lines);
+    Ok(total)
 }
+
+fn part2(line_edit: Vec<String>) -> Result<i32, Box<dyn Error>> {
+    let mut total = 0;
+    for line in line_edit {
+        total += cube_counter::track_minimum_cubes(&line).unwrap();
+    }
+    Ok(total)
+}
+
 
 
 #[cfg(test)]
