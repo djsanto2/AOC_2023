a83e2334634369b13b68c4ea85eb8a299c4e3cea
Daniel Santos <santosdanielj@gmail.com>
2023-12-30 00:27:51 -0500

    day2 part 1 solution implemented

--- day2/src/cube_counter.rs
+++ day2/src/cube_counter.rs
@@ -1,11 +1,8 @@
+use std::collections::VecDeque;
 use std::fs;
 use std::error::Error;
 
-struct bag_of_cubes{
-    red_cubes_ct: i32,
-    green_cubes_ct: i32,
-    blue_cubes_ct: i32,
-}
+
 
 
 pub fn read_doc(filename: &str) -> Result<Vec<String>, Box<dyn Error>>{
@@ -22,5 +19,91 @@ pub fn read_doc(filename: &str) -> Result<Vec<String>, Box<dyn Error>>{
 }
 
 pub fn line_test(line: &str) -> Result<(bool,i32), Box<dyn Error>>{
-    Ok((true,6))
+    
+    let mut game_number = 0;
+    //let bag = bag_of_cubes{red_cubes_ct: 12,green_cubes_ct: 13,blue_cubes_ct:14};
+
+    let mut line_deque: VecDeque<char> = VecDeque::new(); 
+
+    //GET GAME NUMBER FOR counter
+    for c in line.chars(){
+        if !c.is_whitespace(){
+            line_deque.push_back(c);
+        }
+    }
+    let mut current_digits = "".to_string();
+    while *line_deque.front().unwrap() != ':'{
+        current_digits.push(line_deque.pop_front().unwrap());
+    }
+    game_number = current_digits.parse::<i32>().unwrap();
+    line_deque.pop_front(); //pops the :
+
+
+    //check if the game was valid or trickery
+    let mut word = "".to_string();
+    let mut current_count  = 0;
+    let mut toggle = true;
+    while !line_deque.is_empty(){
+        let current_item = line_deque.pop_front().unwrap();
+        if current_item.is_numeric(){
+            word.push(current_item);
+            toggle = true;
+        }
+        else if current_item.is_alphabetic() && !word.is_empty() {
+            if toggle {
+                current_count = word.parse::<i32>().unwrap();
+                word.clear();
+                toggle = false;
+            }
+            word.push(current_item);
+        }
+        else if current_item == ',' || current_item == ';' {
+            if word == "red"{
+                if current_count > 12 {
+                    return Ok((false,game_number))
+                }
+                word.clear();
+                current_count = 0;
+            }
+            else if word == "green" {
+                if current_count > 13 {
+                    return Ok((false,game_number))
+                }
+                word.clear();
+                current_count = 0;
+            }
+            else if word == "blue" {
+                if current_count > 14 {
+                    return Ok((false,game_number))
+                }
+                word.clear();
+                current_count = 0;
+            }
+        }
+        if !word.is_empty(){
+            if word == "red"{
+                if current_count > 12 {
+                    return Ok((false,game_number))
+                }
+                word.clear();
+                current_count = 0;
+            }
+            else if word == "green" {
+                if current_count > 13 {
+                    return Ok((false,game_number))
+                }
+                word.clear();
+                current_count = 0;
+            }
+            else if word == "blue" {
+                if current_count > 14 {
+                    return Ok((false,game_number))
+                }
+                word.clear();
+                current_count = 0;
+            }
+        }
+    }
+
+    Ok((true,game_number))
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,18 +3,40 @@ use cube_counter::*;
 
 
 fn main() {
-    let lines = read_doc("input_shortened.txt").unwrap();
+    let lines = read_doc("input.txt").unwrap();
 
     //let mut total  = 0;
     //let mut gameNumber = 1;
 
     //println!("{:?}", lines);
+    let mut total = 0;
     let mut line_edit: Vec<String>= Vec::new();
     for  line in lines{
         line_edit.push(line[5..].to_string());
     }
     for line in line_edit{
-        let result = cube_counter::line_test(line);
+        let result = cube_counter::line_test(&line).unwrap();
+        if result.0 {
+            total += result.1;
+        }
     }
+    println!("{:?}", total);
+
     //print!("{:?}", lines);
+}
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_1(){
+        assert!(cube_counter::line_test("1: 6 blue, 8 red, 10 green; 12 blue, 10 red;").unwrap() == (true, 1));
+        assert!(cube_counter::line_test("2: 14 blue, 12 red, 13 green;").unwrap() == (true, 2));
+        assert!(cube_counter::line_test("2: 15 blue, 12 red, 13 green;").unwrap() == (false, 2));
+        assert!(cube_counter::line_test("2: 14 blue, 13 red, 13 green;").unwrap() == (false, 2));
+        assert!(cube_counter::line_test("2: 14 blue, 12 red, 14 green;").unwrap() == (false, 2));
+        assert!(cube_counter::line_test("2: 14 blue, 12 red, 13 green; 15 green;").unwrap() == (false, 2));
+    }
 }
