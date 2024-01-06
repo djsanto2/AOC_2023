accc99c4e760c96dd1dccc0aea049614806bb98e
Daniel Santos <santosdanielj@gmail.com>
2024-01-06 18:50:35 -0500

    Removing unnecessary dependencies

--- day5/target/debug/deps/day5-532085109f601ad8.d
+++ day5/target/debug/deps/day5-532085109f601ad8.d
@@ -1,7 +1,6 @@
-C:\Users\santo\Documents\Jobless\AOC_2023\day5\target\debug\deps\libday5-532085109f601ad8.rmeta: src\main.rs src\gardening.rs src\part1.rs
+C:\Users\santo\Documents\Jobless\AOC_2023\day5\target\debug\deps\libday5-532085109f601ad8.rmeta: src\main.rs src\gardening.rs
 
-C:\Users\santo\Documents\Jobless\AOC_2023\day5\target\debug\deps\day5-532085109f601ad8.d: src\main.rs src\gardening.rs src\part1.rs
+C:\Users\santo\Documents\Jobless\AOC_2023\day5\target\debug\deps\day5-532085109f601ad8.d: src\main.rs src\gardening.rs
 
 src\main.rs:
 src\gardening.rs:
-src\part1.rs:
